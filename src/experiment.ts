66074933c8b22c5e0278ab49ad6d4afb508737bc
Okay6 <email2contactme@163.com>
2023-05-17 13:10:28 +0800

    Successfully wrapper function set with return type changed to Promise<T>. ^<>^

--- src/experiment.ts
+++ src/experiment.ts
@@ -1,3 +1,44 @@
+/****************************************************/
+type PromiseWrapper<Value> =
+    Value extends Promise<any> ? Value : Promise<Value>;
+
+
+type wrap<Func, Params> = (...arg: Params) => PromiseWrapper<ReturnType<Func>>
+
+
+type Proxify<T> = {
+    [P in keyof T]: wrap<T[P], Parameters<T[P]>>;
+};
+
+
+type Func = (...args) => {}
+
+export  type  FunctionSet = {
+    [key: string | Symbol]: Func
+}
+
+let funcs = {
+    add: (a: number, b: number): number => a + b,
+    sub: (a: number, b: number): Promise<number> => Promise<number>.resolve(1)
+}
+
+function proxify<T>(o: T): Proxify<T> {
+
+    return
+}
+
+
+function wrapperFunctions(funcSet: FunctionSet): Proxify<FunctionSet> {
+    // 将function set 中的每个function进行包装，而后由proxify对类型进行封装后返回
+
+    return proxify(funcSet)
+}
+
+
+let b = proxify(funcs)
+b.add(1, 2).then(res => console.log(res))
+b.sub(1, 2).then(r => r)
+/****************************************************/
 export type  SubscribeCallBack<I> = {
     // eslint-disable-next-line functional/no-return-void
     readonly onData: (data: I) => void;
@@ -31,20 +72,17 @@ export abstract class ChannelWorkerDefinition<I, O> {
         throw new Error('You should never init this class')
     }
 
-    abstract subscribe(event: MessageEvent<I>): void
+    abstract onmessage(event: MessageEvent<I>): void
 
-    abstract send(data: O): void
-
+    protected postMessage(data: O): void {
+        self.postMessage(data)
+    }
 }
 
 
 class MyWorker extends ChannelWorkerDefinition<number, string> {
 
-    send(data: string): void {
-
-    }
-
-    subscribe(event: MessageEvent<number>): void {
+    onmessage(event: MessageEvent<number>): void {
 
     }
 
@@ -52,13 +90,11 @@ class MyWorker extends ChannelWorkerDefinition<number, string> {
 
 
 export class AccessibleWorkerFactory {
-    public static register<I, O>(_t: new () => ChannelWorkerDefinition<I, O>): IChannelWorkerClient<I, O> {
-        return new ChannelWorkerClient<I, O>();
+    public static register<I, O>(_t: new () => ChannelWorkerDefinition<I, O>): IChannelWorkerClient<O, I> {
+        return new ChannelWorkerClient<O, I>();
     }
 }
 
 
 const a = AccessibleWorkerFactory.register(MyWorker);
-
-a.send('Ok')
 
