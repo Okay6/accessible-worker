39aa29bfab2ef4bb7290b9401dfc239055255d79
Okay6 <email2contactme@163.com>
2023-05-21 10:44:22 +0800

    Add accessible worker template as compile template

---
+++ src/template/accessible_worker.ts
@@ -0,0 +1,17 @@
+export const ACCESSIBLE_WORKER_TEMPLATE = `
+  /**
+   * Accessible Worker
+   */
+   
+  /** Global Variable Zone **/
+        
+  
+  /** Global Method Zone **/
+  
+  
+  /** Message Subscribe **/
+  
+  
+  /** Message Emit **/
+      
+`
